d6daac8fcf7e30373781020f270701aeaeeac42e
Reignsia <142000679+Reignsia@users.noreply.github.com>
2023-10-28 23:17:02 +0800

    he

--- ogpdeb11.sh
+++ ogpdeb11.sh
@@ -8,6 +8,12 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
+config_line="Alias /phpmyadmin /usr/share/phpmyadmin"
+config_file="/etc/apache2/sites-available/000-default.conf"
+new_upload_max_filesize="100M"
+new_post_max_size="100M"
+php_ini_file="/etc/php/7.4/apache2/php.ini"
+
 # Install necessary packages
 apt-get update
 apt-get install -y apache2 curl subversion php7.4 php7.4-gd php7.4-zip libapache2-mod-php7.4 php7.4-curl php7.4-mysql php7.4-xmlrpc php-pear mariadb-server php7.4-mbstring git php-bcmath
@@ -44,6 +50,22 @@ apt-get install -y libstdc++6:i386
 wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/Debian-Ubuntu/ogp-agent-latest.deb" -O "ogp-agent-latest.deb"
 dpkg -i "ogp-agent-latest.deb"
 
+if [ -f "$config_file" ]; then
+    if grep -q "$config_line" "$config_file"; then
+    else
+        sed -i "\$a$config_line" "$config_file"
+    fi
+else
+    echo "File $config_file does not exist."
+fi
+if [ -f "$php_ini_file" ]; then
+    sed -i "s/upload_max_filesize = .*/upload_max_filesize = $new_upload_max_filesize/g" "$php_ini_file"
+    sed -i "s/post_max_size = .*/post_max_size = $new_post_max_size/g" "$php_ini_file"
+    echo "Configuration values updated in $php_ini_file."
+else
+    echo "File $php_ini_file does not exist."
+fi
+(crontab -l ; echo "0 */2 * * * sync && sudo sysctl -w vm.drop_caches=3") | crontab -
 # Clean up
 rm "ogp-panel-latest.deb" "ogp-agent-latest.deb"
 
